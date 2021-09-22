d3189f10733cbf734ce570ffc03caca97d461377
mxw <a@a.com>
2021-09-21 23:15:20 -0400

    unconstrained click to move

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -2,14 +2,17 @@ use crate::prelude::*;
 
 #[system]
 #[read_component(Player)]
+#[read_component(Point)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
 ) {
-    let players = <(Entity, &Point)>::query().filter(component::<Player>());
+    let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
     let mouse_pos = INPUT.lock().mouse_tile(0);
     let mouse_idx = Map::map_idx(mouse_pos.x as usize, mouse_pos.y as usize);
     if INPUT.lock().is_mouse_button_pressed(0){
-        //commands.push(((), WantsToMove{entity: player, destination: mouse_pos}));
+        players.iter(ecs).for_each(|(player, _)| {
+            commands.push(((), WantsToMove{entity: *player, destination: mouse_pos}));
+        });
     }
 }
