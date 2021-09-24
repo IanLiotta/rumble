d0687f16e5e84929701a9fbf566a338273353adc
rustlearn <rust@learn.rs>
2021-09-24 04:59:29 -0400

    Revert "move range highlight"
    This reverts commit 107343bfbc82de625ef26cb945518f8ff241cd9f.

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -15,8 +15,8 @@ pub fn player_input(
 )   
 {
     let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
-    //let mouse_pos = INPUT.lock().mouse_tile(0);
-    //let mouse_idx = Map::map_idx(mouse_pos.x as usize, mouse_pos.y as usize);
+    let mouse_pos = INPUT.lock().mouse_tile(0);
+    let mouse_idx = Map::map_idx(mouse_pos.x as usize, mouse_pos.y as usize);
     // find each player controlled entity
     players.iter(ecs).for_each(|(player, player_pos)| 
     {
@@ -25,12 +25,10 @@ pub fn player_input(
         // queue command to add the MovementRange component to the player entity
         commands.add_component(*player, MovementRange{move_range: possible_moves.clone()});
         // if a valid tile is clicked, queue a message-entity that the player wants to move
-        /*
         if INPUT.lock().is_mouse_button_pressed(0) && possible_moves.contains(&mouse_idx)
         {
             commands.push(((), WantsToMove{entity: *player, destination: mouse_pos}));
             *turn_state = TurnState::PlayerTurn;
         }
-        */
     });
 }
