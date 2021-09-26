b40d01ffc43876ef0f722649f682f10d4f6bc039
rustlearn <rust@learn.rs>
2021-09-26 05:59:37 -0400

    player fov updates

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -4,12 +4,19 @@ use crate::prelude::*;
 // Also the place for synching the camera eventually
 #[system(for_each)]
 #[read_component(Player)]
+#[read_component(FieldOfView)]
 pub fn move_entity(
+    ecs: &SubWorld,
     entity: &Entity,
     want_move: &WantsToMove,
     #[resource] map: &Map,
     commands: &mut CommandBuffer,
 ) {
+    if let Ok(entry) = ecs.entry_ref(want_move.entity) {
+        if let Ok(fov) = entry.get_component::<FieldOfView>() {
+            commands.add_component(want_move.entity, fov.clone_dirty());
+        }
+    }
     if map.can_enter_tile(want_move.destination) {
         let offset_x = (want_move.source.x - want_move.destination.x) as f32;
         let offset_y = (want_move.source.y - want_move.destination.y) as f32;
--- src/systems/render_entity.rs
+++ src/systems/render_entity.rs
@@ -3,18 +3,19 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Render)]
-#[write_component(DrawOffset)]
+#[read_component(DrawOffset)]
 #[read_component(FieldOfView)]
 #[read_component(Player)]
-pub fn render_entity(ecs: &mut SubWorld){
-    let mut renderables = <(&Point, &Render, &mut DrawOffset)>::query();
+pub fn render_entity(ecs: &SubWorld, commands: &mut CommandBuffer){
+    let mut renderables = <(Entity, &Point, &Render, &DrawOffset)>::query();
     let mut fov = <&FieldOfView>::query().filter(component::<Player>());
     let player_fov = fov.iter(ecs).nth(0).unwrap();
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(1);
     renderables
-        .iter_mut(ecs)
-        .for_each(|(pos, render, offset)|{
+        .iter(ecs)
+        .filter(|(_, pos, _, _)| player_fov.visible_tiles.contains(&pos))
+        .for_each(|(entity, pos, render, offset)|{
             let offset_pos_x = pos.x as f32 + offset.offset_x;
             let offset_pos_y = pos.y as f32 + offset.offset_y + 1.; // being drawn off by one with an offset of 0.0, why?
             let offset_pos = PointF::new(offset_pos_x, offset_pos_y);
@@ -26,8 +27,9 @@ pub fn render_entity(ecs: &mut SubWorld){
                 render.color,
                 render.glyph
             ); 
-            if offset.offset_x.abs() >= 0.1 { offset.offset_x -= offset.offset_x.signum()*0.1} else {offset.offset_x = 0.0};
-            if offset.offset_y.abs() >= 0.1 { offset.offset_y -= offset.offset_y.signum()*0.1} else {offset.offset_y = 0.0};
+            let new_offset_x = if offset.offset_x.abs() >= 0.1 { offset.offset_x - offset.offset_x.signum()*0.1} else {0.0};
+            let new_offset_y = if offset.offset_y.abs() >= 0.1 { offset.offset_y - offset.offset_y.signum()*0.1} else {0.0};
+            commands.add_component(*entity, DrawOffset{offset_x: new_offset_x,offset_y:new_offset_y})
 
 
         });
